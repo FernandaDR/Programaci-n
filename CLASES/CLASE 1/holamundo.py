0c187e7c91b484ca17c3dba85d0510d661a5aa4f
FernandaDR <mariafernanda38133@gmail.com>
2020-02-19 12:11:49 -0500

    holamundo

---
+++ CLASES/CLASE 1/holamundo.py
@@ -0,0 +1,20 @@
+#codigo primera clase 
+nombre = "mafe"
+print (nombre)
+print (type(nombre))
+edad = 18
+print (edad)
+print (type(edad))
+dinero = 180000000.00
+print (dinero)
+print (type(dinero))
+nombre = "EMIC"
+print (nombre)
+print (type(nombre))
+antiguedad = 10
+print (antiguedad)
+print (type(antiguedad))
+ganancias = 705230.0202
+print (ganancias)
+print (type(ganancias))
+
