de2e041bbb29e3abd075c845976dacc71c062f30
FernandaDR <mariafernanda38133@gmail.com>
2020-03-29 21:04:52 -0500

    correccion quiz 2

--- examenes1/examen2_covid19.py
+++ examenes1/examen2_covid19.py
@@ -30,60 +30,62 @@ print (MENSAJE_BIENVENIDA)
 pesosPacientesIniciales = [32,64,74,85,98,115,122,127,137,148]
 presionPaciente = []
 
-for _numeroIngresado in range (1,4):
-    while ( _numeroIngresado != 4):
-        print (MENSAJE_MENU)
-        _numeroIngresado = int(input(MENSAJE_NUMERO))
-        if (_numeroIngresado == 1):
-            def mostrar_dos_listas (lista_1,lista_2, mensaje):
-                print(mensaje)
-                posicion = 1
-                for peso in range(len(lista_1)):
-                    presion = peso*6
-                    lista_2.append(presion)
-                    print (lista_1[peso],lista_2[peso])
-                    posicion += 1
-                return()
-            mostrar_dos_listas(pesosPacientesIniciales,presionPaciente, MENSAJE_OPCION_UNO)
 
-        elif (_numeroIngresado == 2 ):
-            def agregarPeso ():
-                print(PREGUNTA_AGREGAR)
+
+     
+
+
+while ( _numeroIngresado != 4):
+    print (MENSAJE_MENU)
+    _numeroIngresado = int(input(MENSAJE_NUMERO))
+    if (_numeroIngresado == 1):
+        def mostrar_dos_listas (lista_1,lista_2, mensaje):
+            print(mensaje)
+            posicion = 1
+            for peso in range(len(lista_1)):
+                presion = peso*6
+                lista_2.append(presion)
+                print (lista_1[peso],lista_2[peso])
+                posicion += 1
+            return()
+        mostrar_dos_listas(pesosPacientesIniciales,presionPaciente, MENSAJE_OPCION_UNO)
+
+    elif (_numeroIngresado == 2 ):
+        def agregarPeso ():
+            print(PREGUNTA_AGREGAR)
+            _pesoAgregado = float(input(PREGUNTA_PESO))
+            pesosPacientesIniciales.append(_pesoAgregado)
+            while(_pesoAgregado != 0.0):
                 _pesoAgregado = float(input(PREGUNTA_PESO))
                 pesosPacientesIniciales.append(_pesoAgregado)
-                while(_pesoAgregado != 0.0):
-                    _pesoAgregado = float(input(PREGUNTA_PESO))
-                    pesosPacientesIniciales.append(_pesoAgregado)
-                    final = NO
-                for i in range (len(pesosPacientesIniciales)):
-                    if (_pesoAgregado == pesosPacientesIniciales[i]):
-                        final = i
-                if (final != NO ):
-                    pesosPacientesIniciales.pop(final)
-                print (pesosPacientesIniciales)
-                return (print(MENSAJE_ACCION_FINALIZADA))
-            agregarPeso()
+                final = NO
+            for i in range (len(pesosPacientesIniciales)):
+                if (_pesoAgregado == pesosPacientesIniciales[i]):
+                    final = i
+            if (final != NO ):
+                pesosPacientesIniciales.pop(final)
+            print (pesosPacientesIniciales)
+            return (print(MENSAJE_ACCION_FINALIZADA))
+        agregarPeso()
 
-        elif (_numeroIngresado == 3):
-            print (MENSAJE_PRESION_MAX,max(presionPaciente))
-            print(MENSAJE_PRESION_MIN,min(presionPaciente))
-            def orden_decreciente (mensaje,lista):
-                print(mensaje)
-                lista.sort(reverse = True )
-                lugar = 1
-                for peso in lista:
-                    print(peso)
-                    lugar += 1
-                return(print("Completo"))
-            orden_decreciente(MENSAJE_LISTA_DECRECIENTE,presionPaciente)
-            print("Pacientes: ",len(presionPaciente))
-            print(presionPaciente)                  
-            print(MENSAJE_CONTINUAR)
-        else: 
-            print(SALIR)
-    print(SALIR)
-    
+    elif (_numeroIngresado == 3):
+        print (MENSAJE_PRESION_MAX,max(presionPaciente))
+        print(MENSAJE_PRESION_MIN,min(presionPaciente))
+        def orden_decreciente (mensaje,lista):
+            print(mensaje)
+            lista.sort(reverse = True )
+            lugar = 1
+            for peso in lista:
+                print(peso)
+                lugar += 1
+            return(print("Completo"))
+        orden_decreciente(MENSAJE_LISTA_DECRECIENTE,presionPaciente)
+        print("Pacientes: ",len(presionPaciente))                 
+        print(MENSAJE_CONTINUAR)
+    else: 
+        print(SALIR)
 
 
 
 
+  
